8e65f32435c8c5e154ed980bc18db4bcda64411e
zengchanghuan <zengchanghuan@gmail.com>
2022-06-22 17:22:23 +0800

    add go code

--- go_blog/main.go
+++ go_blog/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 type IndexData struct {
@@ -20,11 +22,24 @@ func index(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonStr)
 }
 
+func indexHtml(w http.ResponseWriter, r *http.Request) {
+	var indexData IndexData
+	indexData.Title = "go learning"
+	indexData.Desc = "go code"
+	t := template.New("index.html")
+	//拿到当前路径
+	path, _ := os.Getwd()
+	t, _ = t.ParseFiles(path + "/template/index.html")
+	t.Execute(w, indexData)
+
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/", index)
+	http.HandleFunc("/index.html", indexHtml)
 	error := server.ListenAndServe()
 	if error != nil {
 		log.Println(error)
