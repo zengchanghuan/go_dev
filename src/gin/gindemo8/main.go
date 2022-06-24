ab2d5761c674d921153301332190157c385e271b
zengchanghuan <zengchanghuan@gmail.com>
2022-06-24 10:46:26 +0800

    全局中间件

--- src/gin/gindemo8/main.go
+++ src/gin/gindemo8/main.go
@@ -2,28 +2,18 @@ package main
 
 import (
 	"fmt"
+	"gindemo8/routers"
 	"html/template"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-// UnixToTime 时间戳转换成日期
+//时间戳转换成日期Ï
 func UnixToTime(timestamp int) string {
 	fmt.Println(timestamp)
 	t := time.Unix(int64(timestamp), 0)
 	return t.Format("2006-01-02 15:04:05")
-}
-func initMiddleware(c *gin.Context) {
-	start := time.Now().UnixNano()
-	fmt.Println("1-我是一个中间件")
-	//调用该请求的剩余处理程序
-	c.Next()
-
-	fmt.Println("2-我是一个中间件")
-	end := time.Now().UnixNano()
-
-	fmt.Println(end - start)
 }
 
 func main() {
@@ -38,18 +28,11 @@ func main() {
 	//配置静态web目录   第一个参数表示路由, 第二个参数表示映射的目录
 	r.Static("/static", "./static")
 
-	r.GET("/", initMiddleware, func(c *gin.Context) {
-		fmt.Println("这是一个首页")
-		// time.Sleep(time.Second)
-		c.String(200, "gin 首页 hello")
-	})
+	routers.AdminRoutersInit(r)
 
-	r.GET("/news", initMiddleware, func(c *gin.Context) {
-		c.String(200, "新闻页面")
-	})
-	r.GET("/login", initMiddleware, func(c *gin.Context) {
-		c.String(200, "login")
-	})
+	routers.ApiRoutersInit(r)
+
+	routers.DefaultRoutersInit(r)
 
 	r.Run()
 }
---
+++ src/gin/gindemo8/routers/defaultRouters.go
@@ -0,0 +1,14 @@
+package routers
+
+import (
+	"gindemo8/controllers/itying"
+	"github.com/gin-gonic/gin"
+)
+
+func DefaultRoutersInit(r *gin.Engine) {
+	defaultRouters := r.Group("/")
+	{
+		defaultRouters.GET("/", itying.DefaultController{}.Index)
+		defaultRouters.GET("/news", itying.DefaultController{}.News)
+	}
+}
