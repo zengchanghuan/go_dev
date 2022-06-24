264268a96b6acf09ffb6969a0c53089b4e7c1438
zengchanghuan <zengchanghuan@gmail.com>
2022-06-24 19:52:52 +0800

    设置session的过期时间

--- src/gin/gindemo13/controllers/itying/defaultController.go
+++ src/gin/gindemo13/controllers/itying/defaultController.go
@@ -12,6 +12,10 @@ type DefaultController struct{}
 func (con DefaultController) Index(c *gin.Context) {
 	//设置sessions
 	session := sessions.Default(c)
+	//配置session的过期时间
+	session.Options(sessions.Options{
+		MaxAge: 3600 * 6, // 6hrs   MaxAge单位是秒
+	})
 	session.Set("username", "张三 111")
 	session.Save() //设置session的时候必须调用
 
