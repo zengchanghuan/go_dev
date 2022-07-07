3181b18527a02ec7fd900d0e48a3aafaa93824dc
zengchanghuan <zengchanghuan@gmail.com>
2022-07-07 20:00:46 +0800

    comit

--- src/gin/gindemo16/controllers/admin/articleController.go
+++ src/gin/gindemo16/controllers/admin/articleController.go
@@ -14,25 +14,25 @@ type ArticleController struct {
 func (con ArticleController) Index(c *gin.Context) {
 
 	//获取所有的文章
-	// articleList := []models.Article{}
-	// models.DB.Find(&articleList)
-	// c.JSON(200, gin.H{
-	// 	"result": articleList,
-	// })
+	articleList := []models.Article{}
+	models.DB.Find(&articleList)
+	c.JSON(200, gin.H{
+		"result": articleList,
+	})
 
 	// 查询文章 获取文章对应的分类
-	// articleList := []models.Article{}
-	// models.DB.Preload("ArticleCate").Find(&articleList)
-	// c.JSON(200, gin.H{
-	// 	"result": articleList,
-	// })
+	//articleList := []models.Article{}
+	//models.DB.Preload("ArticleCate").Find(&articleList)
+	//c.JSON(200, gin.H{
+	//	"result": articleList,
+	//})
 
 	//获取所有的分类
-	articleCateList := []models.ArticleCate{}
-	models.DB.Preload("Article").Find(&articleCateList)
-	c.JSON(200, gin.H{
-		"result": articleCateList,
-	})
+	//articleCateList := []models.ArticleCate{}
+	//models.DB.Preload("Article").Find(&articleCateList)
+	//c.JSON(200, gin.H{
+	//	"result": articleCateList,
+	//})
 
 }
 func (con ArticleController) Add(c *gin.Context) {
--- src/gin/gindemo16/models/articleCate.go
+++ src/gin/gindemo16/models/articleCate.go
@@ -1,6 +1,6 @@
 package models
 
-//foreignKey外键  如果是表名称加上Id的话默认也可以不配置   如果不是我们需要通过foreignKey配置外键
+// ArticleCate foreignKey外键  如果是表名称加上Id的话默认也可以不配置   如果不是我们需要通过foreignKey配置外键
 //references表示的是主键    默认就是Id   如果是Id的话可以不配置
 type ArticleCate struct {
 	Id      int //主键
@@ -9,7 +9,7 @@ type ArticleCate struct {
 	Article []Article `gorm:"foreignKey:CateId;references:Id"`
 }
 
-//表示配置操作数据库的表名称
+// TableName 表示配置操作数据库表名
 func (ArticleCate) TableName() string {
 	return "article_cate"
 }
